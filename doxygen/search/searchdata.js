e654d60a903bba449e831015caea7120298004bd
zerolh0 <zerolh0@users.noreply.github.com>
2024-02-02 05:55:21 +0000

    deploy: 438e0abd351f5398bd1c5d770372c42355fd26ac

---
+++ doxygen/search/searchdata.js
@@ -0,0 +1,39 @@
+var indexSectionsWithContent =
+{
+  0: "ads",
+  1: "s",
+  2: "s",
+  3: "s",
+  4: "s",
+  5: "s",
+  6: "s",
+  7: "s",
+  8: "ad"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files",
+  2: "functions",
+  3: "variables",
+  4: "typedefs",
+  5: "enums",
+  6: "enumvalues",
+  7: "defines",
+  8: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Files",
+  2: "Functions",
+  3: "Variables",
+  4: "Typedefs",
+  5: "Enumerations",
+  6: "Enumerator",
+  7: "Macros",
+  8: "Pages"
+};
+
